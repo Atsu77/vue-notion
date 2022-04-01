a64334af1047365ecff8020a3104fffc2ce89c0e
Atsu <kamnknon@gmail.com>
2022-04-01 20:49:30 +0900

    create navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,7 +1,36 @@
 <template>
-  <div class="navigation-bar">
-    ナビゲーションバーです
+<!-- ナビゲーションバーをダークテーマで設定 -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <!-- タイトル -->
+  <a class="navbar-brand" href="#">Notion</a>
+
+  <!-- 画面サイズに応じて折りたたむ設定 -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <!-- メニュー全体 -->
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav">
+      <!-- ノートリストページへのリンク -->
+      <li class="nav-item">
+        <a class="nav-link" href="#"><font-awesome-icon icon="fa-solid fa-file-lines" /> ノートリスト</a>
+      </li>
+
+      <!-- 学習用ページへのリンク -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <font-awesome-icon icon="fa-solid fa-user-graduate" /> 学習用ページ
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><a class="dropdown-item" href="#">学習1</a></li>
+          <li><a class="dropdown-item" href="#">学習2</a></li>
+          <li><a class="dropdown-item" href="#">学習3</a></li>
+        </ul>
+      </li>
+    </ul>
   </div>
+</nav>
 </template>
 
 <script>
@@ -10,9 +39,4 @@ export default {
 </script>
 
 <style scoped>
-.navigation-bar {
-  height: 60px;
-  color: #fff;
-  background-color: #6c757d;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/* BootStrapの導入 */
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+/* Font Awesomeの導入 */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons'
+import { faFileLines } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+
+library.add(faUserGraduate);
+library.add(faFileLines);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 Vue.config.productionTip = false
 
 new Vue({
