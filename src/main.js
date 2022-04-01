96137eab792aa0dd845f29c3a3a3da1cdebef2e8
Atsu <kamnknon@gmail.com>
2022-04-01 21:10:39 +0900

    install VueRouter

--- src/main.js
+++ src/main.js
@@ -1,26 +1,37 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
 /* BootStrapの導入 */
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 /* Font Awesomeの導入 */
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserGraduate } from '@fortawesome/free-solid-svg-icons'
-import { faFileLines } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faUserGraduate } from "@fortawesome/free-solid-svg-icons";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+/*  BootStrapの設定 */
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-
+/* Font Awesomeの設定 */
 library.add(faUserGraduate);
 library.add(faFileLines);
-Vue.component('font-awesome-icon', FontAwesomeIcon)
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+/* VueRouterを使用 */
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  route: [],
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
